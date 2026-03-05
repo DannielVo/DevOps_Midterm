fae8b9de33ed57dd2ec2a0c36ba03923c2a1fde6
DannielVo <phattinhoc2017@gmail.com>
2026-03-05 21:43:34 +0700

    Feat: Finish product validators

--- phase1/source-code/validators/productValidator.js
+++ phase1/source-code/validators/productValidator.js
@@ -1,13 +1,28 @@
-// Core code
-
+// Code feature
 const { body } = require("express-validator");
 
-// Code feature validation (standby)
+const create = [
+  body("name").isString().isLength({ min: 1 }),
+  body("price").isNumeric(),
+  body("color").isString().isLength({ min: 1 }),
+  body("description").optional().isString().isLength({ max: 5000 }),
+  body("imageUrl").optional().isString().isLength({ max: 500000 }),
+];
 
-const create = [(req, res, next) => next()];
+const put = [
+  body("name").exists().isString().isLength({ min: 1 }),
+  body("price").exists().isNumeric(),
+  body("color").exists().isString().isLength({ min: 1 }),
+  body("description").optional().isString().isLength({ max: 5000 }),
+  body("imageUrl").optional().isString().isLength({ max: 500000 }),
+];
 
-const put = [(req, res, next) => next()];
-
-const patch = [(req, res, next) => next()];
+const patch = [
+  body("name").optional().isString().isLength({ min: 1 }),
+  body("price").optional().isNumeric(),
+  body("color").optional().isString().isLength({ min: 1 }),
+  body("description").optional().isString().isLength({ max: 5000 }),
+  body("imageUrl").optional().isString().isLength({ max: 500000 }),
+];
 
 module.exports = { create, put, patch };
